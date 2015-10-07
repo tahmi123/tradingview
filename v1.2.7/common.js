148ce77c9bfa9ac778afef9bc4ecf019804eb91b
Arnab Karmakar <arnab@binary.com>
2015-10-07 06:36:06 +0000

    Commiting v1.2.7 using TravisCI and GruntJS build process for prod

--- v1.2.7/common.js
+++ v1.2.7/common.js
@@ -1,6 +1,1 @@
-function getParameterByName(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-      results = regex.exec(location.search);
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+function getParameterByName(a){a=a.replace(/[\[]/,"\\[").replace(/[\]]/,"\\]");var b=new RegExp("[\\?&]"+a+"=([^&#]*)"),c=b.exec(location.search);return null===c?"":decodeURIComponent(c[1].replace(/\+/g," "))}
